d2498ca756a74fe931e36cedc5f90706a637e5ff
Tekutos <tekuto1993@gmail.com>
2022-12-06 14:47:24 +0300

    repair switch

--- my-app/src/components/Navigation/Nav.jsx
+++ my-app/src/components/Navigation/Nav.jsx
@@ -1,28 +1,44 @@
-import { React, useState } from "react";
+import { useEffect, useState } from "react";
+import { React } from "react";
 import "./Nav.scss";
 
 function Nav() {
   const base = `nav`;
 
-  // const [state, setState] = useState("");
+  useEffect(() => {
+    const list = document.querySelectorAll(".nav_list");
+    function activeLink() {
+      list.forEach((item) => item.classList.remove("active"));
+      this.classList.add("active");
+    }
+    list.forEach((item) => item.addEventListener("click", activeLink));
+  }, []);
 
-  const list = document.querySelectorAll(".nav_list");
-  function activeLink() {
-    list.forEach((item) => item.classList.remove("active"));
-    this.classList.add("active");
-  }
-  list.forEach((item) => item.addEventListener("click", activeLink));
+  // const inActive = (e) => {
+  //   e.preventDefault();
+  //   e.target.classList.contains("active")
+  //     ? e.target.classList.remove("active")
+  //     : e.target.classList.add("active");
+  //   console.log(e.target);
+  // };
 
-  // ${({ inActive }) => inActive ? "active" : undefined}
-  console.log(list);
+  // useEffect(
+  //   (e) => {
+  //     e.preventDefault();
+  //     e.target.classList.contains("active")
+  //       ? e.target.classList.remove("active")
+  //       : e.target.classList.add("active");
+  //   },
+  //   []
+  // );
 
   return (
     <div className={`${base}`}>
       <div className={`${base}_navigation`}>
         <ul className={`${base}_ul`}>
-          <li className={`${base}_list active `}>
+          <li className={`${base}_list active`}>
             <a href="#">
-              <span className={`${base}_icon`}>
+              <span className={`${base}_icon `}>
                 <ion-icon name="home-outline"></ion-icon>
               </span>
               <span className={`${base}_text`}>Home</span>
